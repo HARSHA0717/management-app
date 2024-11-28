bcf3ef6fca57267c6971c245b3788e922e766210
HARSHA0717 <harshamummadi0709@gmail.com>
2024-11-28 14:04:29 +0530

    completed creating login form and login authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthenticationGuard } from './authentication.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'',component:LoginComponent},
+  {path:'dashboard',canActivate:[AuthenticationGuard],component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
